a5739521a0c3a351b26ec0baeaa816e2ab03f502
Alisher <alishersharipov670@gmail.com>
2025-05-26 22:20:38 +0500

    update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,47 +1,49 @@
-import React, { ChangeEvent, FormEvent, MouseEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
 
 const Header = () => {
-    const [count, setCount] = useState<number>(0)
-    const [searchValue, setSearchValue] = useState<string>("")
+   const [count, setCount] = useState<number>(0);
+   const [searchValue, setSearchValue] = useState<string>("");
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
 
-        let formData = new FormData(event.target as HTMLFormElement)
-        let body = Object.fromEntries(formData)
+      let formData = new FormData(event.target as HTMLFormElement);
+      let body = Object.fromEntries(formData);
 
-        console.log(body);
+      console.log(body);
 
-        (event.target as HTMLFormElement).reset() 
-    }
+      (event.target as HTMLFormElement).reset();
+   };
 
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-        console.log(e.currentTarget.innerHTML);
-    }
+   const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+      console.log(e.currentTarget.innerHTML);
+   };
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearchValue(e.target.value)
-    }
+   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearchValue(e.target.value);
+   };
 
-  return (
-    <div>
-        <h2>Header {count} {searchValue}</h2>
-        <button onClick={()=> setCount((p) => p + 1)}>increment</button>
+   return (
+      <div>
+         <h2>
+            Header {count} {searchValue}
+         </h2>
+         <button onClick={() => setCount((p) => p + 1)}>increment</button>
 
-        <input type="search" onChange={handleChange} value={searchValue} />
+         <input type='search' onChange={handleChange} value={searchValue} />
 
-        <form onSubmit={handleSubmit} action="">
-            <input name='fname' type="text" />
-            <input name='lname' type="text" />
-            <input name='username' type="text" />
-            <input name='parol' type="password" />
+         <form onSubmit={handleSubmit} action=''>
+            <input name='fname' type='text' />
+            <input name='lname' type='text' />
+            <input name='username' type='text' />
+            <input name='parol' type='password' />
             <button>submit</button>
-        </form>
-        <button onClick={handleClick}>1</button>
-        <button onClick={handleClick}>2</button>
-        <button onClick={handleClick}>3</button>
-    </div>
-  )
-}
+         </form>
+         <button onClick={handleClick}>1</button>
+         <button onClick={handleClick}>2</button>
+         <button onClick={handleClick}>3</button>
+      </div>
+   );
+};
 
-export default React.memo(Header)
+export default React.memo(Header);
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { RootState } from '../../redux'
-import { decrement, inc, incrementByAmount } from '../../redux/features/counter.slice'
+import React from "react";
 
 // useSelector - get
 // useDipatch - set
 
 const Hero = () => {
-  // const [state, dispatch] = useStateValue()
-  const count = useSelector((state: RootState)=> state.counter.value)
-  const dispatch = useDispatch()
+   // const [state, dispatch] = useStateValue()
+   //  const count = useSelector((state: RootState) => state.counter.value);
+   //  const dispatch = useDispatch();
 
-  return (
-    <div>
-        <h2>Hero {count}</h2>
-        {/* <button onClick={()=> dispatch({type:"INC", payload: 5})}>increment</button> */}
-        <button onClick={()=> dispatch(inc())}>increment</button>
-        <button onClick={()=> dispatch(decrement())}>decrement</button>
-        <button onClick={()=> dispatch(incrementByAmount(5))}>incrementByAmount</button>
+   return (
+      <div>
+         <h2>Hero</h2>
+         {/* <button onClick={()=> dispatch({type:"INC", payload: 5})}>increment</button> */}
+         {/* <button onClick={() => dispatch(inc())}>increment</button>
+         <button onClick={() => dispatch(decrement())}>decrement</button>
+         <button onClick={() => dispatch(incrementByAmount(5))}>incrementByAmount</button> */}
+      </div>
+   );
+};
 
-    </div>
-  )
-}
-
-export default React.memo(Hero)
+export default React.memo(Hero);
